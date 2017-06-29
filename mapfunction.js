66d5f829b30f463a7dcb3033cede02ccd75c4471
Hannah van Adrichem <hannah.vanadrichem@gmail.com>
2017-06-29 20:29:20 +0000

    created map function to return array based on results of callback function

---
+++ mapfunction.js
@@ -0,0 +1,20 @@
+// Implement your own version of the built-in array map function.
+
+// Yours will take in two arguments.
+// The first will be an array to map
+// and the second will be a callback function.
+// The function will return a new array
+// based on the results of the callback function.
+
+var words = ["ground", "control", "to", "major", "tom"];
+
+map(words, function(word) {return word.length});
+
+function map(array, callback){
+  var newArray = [];
+  array.forEach(function(wordInList){
+    newArray.push(callback(wordInList));
+  });
+  return newArray;
+};
+
